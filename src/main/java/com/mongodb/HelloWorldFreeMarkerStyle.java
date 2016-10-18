aa99882dcdce7ed88bf5f26d2c809064965b738d
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2016-10-18 22:24:43 +0100

    Introducing FreeMarker alone.

---
+++ src/main/java/com/mongodb/HelloWorldFreeMarkerStyle.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mongodb;
+
+import freemarker.cache.FileTemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author msultan
+ */
+public class HelloWorldFreeMarkerStyle {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldFreeMarkerStyle.class, "/");
+
+        try {
+            configuration.setDirectoryForTemplateLoading(new File(
+                    ".\\src\\main\\java\\resources"));
+            Template helloTemplate = configuration.getTemplate("hello.ftl");
+            StringWriter writer = new StringWriter();
+            Map<String, Object> helloMap = new HashMap<String, Object>();
+            helloMap.put("name", "FreeMarker");
+
+            helloTemplate.process(helloMap, writer);
+            System.out.println(writer);
+
+        } catch (Exception ex) {
+            Logger.getLogger(HelloWorldFreeMarkerStyle.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
