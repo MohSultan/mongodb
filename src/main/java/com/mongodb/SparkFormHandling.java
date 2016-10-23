f74850efc05127ed93fa198ccb46d25193eb0abe
mohamed Sultan <mohamed.sultan1581@gmail.com>
2016-10-23 19:53:07 +0100

    Added Get/Post Spark

---
+++ src/main/java/com/mongodb/SparkFormHandling.java
@@ -0,0 +1,58 @@
+package com.mongodb;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import java.io.File;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class SparkFormHandling {
+
+    public static void main(String[] args) {
+        // Configure Freemarker
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(SparkFormHandling.class, "/");
+
+        // Configure routes
+        Spark.get("/", new Route() {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                try {
+                    configuration.setDirectoryForTemplateLoading(new File(
+                            ".\\src\\main\\java\\resources"));
+                    Map<String, Object> fruitsMap = new HashMap<String, Object>();
+                    fruitsMap.put("fruits",
+                            Arrays.asList("apple", "orange", "banana", "peach"));
+
+                    Template fruitPickerTemplate
+                            = configuration.getTemplate("fruitPicker.ftl");
+                    StringWriter writer = new StringWriter();
+                    fruitPickerTemplate.process(fruitsMap, writer);
+                    return writer;
+
+                } catch (Exception e) {
+//                    halt(500);
+                    return null;
+                }
+            }
+        });
+
+        Spark.post("/favorite_fruit", new Route() {
+            @Override
+            public Object handle(final Request request, final Response response) {
+                final String fruit = request.queryParams("fruit");
+                if (fruit == null) {
+                    return "Why don't you pick one?";
+                } else {
+                    return "Your favorite fruit is " + fruit;
+                }
+            }
+        });
+    }
+}
