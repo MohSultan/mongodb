6cbef24a8822d9032127c6faaec5ba5b62b8ee2b
mohamed Sultan <mohamed.sultan1581@gmail.com>
2016-10-23 19:08:38 +0100

    Added HelloWorldSparkFreemarkerStyle

--- src/main/java/com/mongodb/HelloWorldSparkFreemarkerStyle.java
+++ src/main/java/com/mongodb/HelloWorldSparkFreemarkerStyle.java
@@ -1,34 +1,41 @@
-/*
- * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.mongodb;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import java.io.File;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Spark;
 
-public class HelloWorldSparkStyle {
+public class HelloWorldSparkFreemarkerStyle {
     public static void main(String[] args) {
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldSparkFreemarkerStyle.class, "/");
         Spark.get("/",new Route() {
             @Override
             public Object handle(final Request request,
-                                 final Response response) {
-                return "Hello World From Spark\n";
+                    final Response response) {
+                StringWriter writer = new StringWriter();
+                try {
+                    configuration.setDirectoryForTemplateLoading(new File(
+                            ".\\src\\main\\java\\resources"));
+                    Template helloTemplate = configuration.getTemplate("hello.ftl");
+                    Map<String, Object> helloMap = new HashMap<String, Object>();
+                    helloMap.put("name", "FreeMarker Spark");
+
+                    helloTemplate.process(helloMap, writer);
+                    System.out.println(writer);
+
+                } catch (Exception ex) {
+                    Logger.getLogger(HelloWorldFreeMarkerStyle.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                return writer;
             }
         });
     }
