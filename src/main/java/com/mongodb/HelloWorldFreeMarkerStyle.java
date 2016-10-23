ddfa90ade429da0fb3a0533493fde647b322c530
mohamed Sultan <mohamed.sultan1581@gmail.com>
2016-10-23 18:47:32 +0100

    Removed unused lines.

--- src/main/java/com/mongodb/HelloWorldFreeMarkerStyle.java
+++ src/main/java/com/mongodb/HelloWorldFreeMarkerStyle.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mongodb;
 
-import freemarker.cache.FileTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import java.io.File;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
