195a139f07c825fb47ff8321337d2f21c9f787fc
Mohamed Sultan <mohamed.sultan1581@gmail.com>
2016-10-29 12:38:41 +0100

    Solution of 3.3

--- Week2_HW/src/main/java/course/BlogPostDAO.java
+++ Week2_HW/src/main/java/course/BlogPostDAO.java
@@ -4,6 +4,9 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Updates;
+import freemarker.template.SimpleHash;
 import java.util.ArrayList;
 import java.util.Date;
 import org.bson.Document;
@@ -98,5 +101,17 @@ public class BlogPostDAO {
         // - email is optional and may come in NULL. Check for that.
         // - best solution uses an update command to the database and a suitable
         //   operator to append the comment on to any existing list of comments
+        List comments = new ArrayList<Object>();
+        Document doc = new Document();
+        doc.append("author", name).append("body", body);
+
+        if (email != null) {
+            doc.append("email", email);
+        }
+        comments.add(doc);
+//        postsCollection.updateMany(new BasicDBObject("permalink", permalink), Updates.unset("comments"));
+        postsCollection.updateMany(new BasicDBObject("permalink", permalink),
+                Updates.pushEach("comments", comments));
+
     }
 }
